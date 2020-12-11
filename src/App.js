51cdaad151bade775ea5ef837a73b76144ac1c9d
funhappyit <fun.happyit@gmail.com>
2020-12-11 12:48:26 +0900

    2020-12-11 한유경 useReducer 커밋

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Info from "./Info";
+import { useState } from "react";
+import Counter from "./Counter";
 
-function App() {
-  return <Info />;
-}
+const App = () => {
+  /*
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"};
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
+  */
+  return <Counter />;
+};
 
 export default App;
